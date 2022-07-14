ceb5d79c4104ec0014d7b2eb8250213735449fd0
Cruz Maldonado <cruzmaldonadocharris@gmail.com>
2022-07-14 16:07:18 -0400

    añadiendo algo de responsividad al portafolio

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+const menuMobile=document.getElementById("menu-mobile");
+const menu=document.getElementById("menu");
+
+menuMobile.addEventListener('click',()=>{
+    menu.classList.toggle('main-menu-show');
+})
