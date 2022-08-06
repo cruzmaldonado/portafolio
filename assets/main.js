db6659e6d6863a13844193385ab6f718912a8dcd
Cruz Maldonado <cruzmaldonadocharris@gmail.com>
2022-08-06 13:08:57 -0400

    arreglando el problema del margen del navegador

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,7 @@
 const menuMobile=document.getElementById("menu-mobile");
 const menu=document.getElementById("menu");
 
+
 menuMobile.addEventListener('click',()=>{
     menu.classList.toggle('main-menu-show');
 })
